be4812066d608c8d850970e11e94bf4b2b382694
Shubham_Gajera <106212948+Shubham09876543@users.noreply.github.com>
2025-04-02 19:09:46 +0530

    Add sign in and log in server.js with postgre

--- Fertilizer360-main/server.js
+++ Fertilizer360-main/server.js
@@ -101,5 +101,19 @@ app.post("/login", async (req, res) => {
 });
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
